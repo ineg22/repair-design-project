e184ca31e650827d5b97bda96362cc8f131d434e
ineg22 <ineg22@gmail.com>
2019-10-16 17:03:13 +0300

    feat: add mobile slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 'use strict'
 
+const slider1 = document.querySelector("#mini-slider-1");
+const slider1Left = document.querySelector("#mini-slider-1__left");
+const slider1Right = document.querySelector("#mini-slider-1__right");
+const slider2 = document.querySelector("#mini-slider-2");
+const slider2Left = document.querySelector("#mini-slider-2__left");
+const slider2Right = document.querySelector("#mini-slider-2__right");
+
+slider1Left.addEventListener('click', moveSlideLeft(slider1, slider1Left, slider1Right));
+slider1Right.addEventListener('click', moveSlideRight(slider1, slider1Left, slider1Right));
+slider2Left.addEventListener('click', moveSlideLeft(slider2, slider2Left, slider2Right));
+slider2Right.addEventListener('click', moveSlideRight(slider2, slider2Left, slider2Right));
+
+function moveSlideLeft(slider, left, right) {
+  return function () {
+    if (slider.classList[1] === 'mini-slider__second-pos') {
+      slider.classList.remove('mini-slider__second-pos');
+      slider.classList.add('mini-slider__first-pos');
+      left.classList.add('hidden');
+    } else if (slider.classList[1] === 'mini-slider__third-pos') {
+      slider.classList.remove('mini-slider__third-pos');
+      slider.classList.add('mini-slider__second-pos');
+      right.classList.remove('hidden');
+    }
+  }
+}
+
+function moveSlideRight(slider, left, right) {
+  return function() {
+    if (slider.classList[1] === 'mini-slider__second-pos') {
+      slider.classList.remove('mini-slider__second-pos');
+      slider.classList.add('mini-slider__third-pos');
+      right.classList.add('hidden');
+    } else if (slider.classList[1] === 'mini-slider__first-pos') {
+      slider.classList.remove('mini-slider__first-pos');
+      slider.classList.add('mini-slider__second-pos');
+      left.classList.remove('hidden');
+    }
+  }
+}
+
+document.querySelector('.completed-projects').addEventListener('click', slideSelect);
+
 function slideSelect(evt) {
   evt.stopPropagation();
 
@@ -15,5 +57,3 @@ function slideSelect(evt) {
     }
   }, 0);
 }
-
-document.querySelector('.completed-projects').addEventListener('click', slideSelect);
